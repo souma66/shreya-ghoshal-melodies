07a15fc1f04ea1ec916f524f9e25319527b82f1a
Soumadip Chakraborty <soumadipchakraborty509@gmail.com>
2026-08-17 09:14:16 +0530

    Add Google Search Console verification

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -4,8 +4,8 @@ export const Branding: React.FC = React.memo(() => {
   return (
     <header className="brand-header-top-left" role="banner">
       <div className="brand-mark">
-        <h1 className="brand-name">SHREYA GHOSHAL</h1>
-        <span className="brand-tag">MELODIES</span>
+        <h1 className="brand-name">Shreya Ghoshal Melodies</h1>
+        <span className="brand-tag">A CINEMATIC LISTENING EXPERIENCE</span>
       </div>
     </header>
   );
